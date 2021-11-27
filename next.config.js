f8c5503e6d4846043577a663eba18afdbb08959b
Jean <jean.carlosmolossi@gmail.com>
2021-11-27 01:37:39 -0300

    chore: alteradas configuracoes de next e docker

--- next.config.js
+++ next.config.js
@@ -1,17 +1,22 @@
 const path = require('path');
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src', 'styles')],
-  },
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/login",
-        permanent: true
-      }
-    ]
-  }
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [
+            path.join(__dirname, 'src', 'presentation', 'styles'),
+            path.join(__dirname, 'src', 'presentation', 'styles', 'components'),
+            path.join(__dirname, 'src', 'presentation', 'styles', 'pages'),
+            path.join(__dirname, 'src', 'presentation', 'styles', 'shared'),
+        ],
+    },
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/login",
+                permanent: true
+            }
+        ]
+    }
 }
