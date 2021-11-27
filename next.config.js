31aa559618252a7aef2f493f8cb2f3eb26fa5c95
Jean <jean.carlosmolossi@gmail.com>
2021-11-27 00:08:05 -0300

    chore: ajustes em arquivos de configuração

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
+  },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true
+      }
+    ]
+  }
 }
