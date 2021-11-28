4cab60e6fff3030ca2d7fe9a855b98b9a63fef7b
Jean <jean.carlosmolossi@gmail.com>
2021-11-28 00:08:44 -0300

    feat: adicionado tema do Material UI

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../presentation/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import '../presentation/styles/globals.scss'
+import { theme } from "@/presentation/styles/shared/theme";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp( { Component, pageProps }: AppProps ) {
+	return (
+		<>
+			<ThemeProvider theme={ theme }>
+				<CssBaseline/>
+				<Component { ...pageProps } />
+			</ThemeProvider>
+		</>
+	)
 }
 
 export default MyApp
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -24,6 +24,7 @@ class MyDocument extends Document {
 						href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;700&display=swap"
 						rel="stylesheet"
 					/>
+					<meta name="viewport" content="initial-scale=1, width=device-width" />
 				</Head>
 				<body>
 				<Main/>
