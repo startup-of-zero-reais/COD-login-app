9fecaa30dc8e28aaf2fff4824a99f2d3b9bf86b1
Jean <jean.carlosmolossi@gmail.com>
2021-11-28 01:06:54 -0300

    refactor: ajustes no _app e no _document

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,37 @@
+import { useEffect } from "react";
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import '../presentation/styles/globals.scss'
 import { theme } from "@/presentation/styles/shared/theme";
+import { EmotionCache } from "@emotion/utils";
+import { createEmotionCache } from "@/presentation/utils";
+import { CacheProvider } from "@emotion/react";
 
-function MyApp( { Component, pageProps }: AppProps ) {
-	return (
-		<>
-			<ThemeProvider theme={ theme }>
-				<CssBaseline/>
-				<Component { ...pageProps } />
-			</ThemeProvider>
-		</>
-	)
+interface MyAppProps extends AppProps {
+    emotionCache?: EmotionCache
+}
+
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp( { Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps ) {
+    useEffect(() => {
+        // Remove the server-side injected CSS.
+        const jssStyles = document.querySelector("#jss-server-side");
+        if (jssStyles) {
+            jssStyles.parentElement?.removeChild(jssStyles);
+        }
+    }, []);
+
+    return (
+        <>
+            <CacheProvider value={ emotionCache }>
+                <ThemeProvider theme={ theme }>
+                    <CssBaseline/>
+                    <Component { ...pageProps } />
+                </ThemeProvider>
+            </CacheProvider>
+        </>
+    )
 }
 
 export default MyApp
