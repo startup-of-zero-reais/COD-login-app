4210357353a0c56cd1f52920bbb05dcad27bad21
Jean <jean.carlosmolossi@gmail.com>
2021-12-03 22:04:24 -0300

    refactor: ajuste no disparo de snackbars

--- src/presentation/hooks/shared/use-error-polyfill.ts
+++ src/presentation/hooks/shared/use-error-polyfill.ts
@@ -1,7 +1,14 @@
 import { Dispatch, SetStateAction, useCallback } from "react";
 import { HandleRequestError } from "@/data/protocols/local-handler";
+import { useSnackbarContext } from "@/presentation/contexts/snackbar-stack";
 
 export function useErrorPolyfill() {
+	const { openSnackbar } = useSnackbarContext()
+
+	const getMessageIfExists = useCallback(( obj: object ) => {
+		return Object.getOwnPropertyNames(obj).find(prop => prop === "message")
+	}, [])
+
 	const polyfill = useCallback(<T = any, S = any>( errs: HandleRequestError<T>, setErrors: Dispatch<SetStateAction<S>> ) => {
 		if (errs) {
 			if (typeof errs === "string") {
@@ -9,6 +16,11 @@ export function useErrorPolyfill() {
 					...prevState,
 					formErrors: [ errs ]
 				}))
+
+				openSnackbar({
+					type: "error",
+					message: errs
+				})
 
 				return false
 			}
@@ -19,13 +31,21 @@ export function useErrorPolyfill() {
 					formErrors: errs
 				}))
 
+				openSnackbar({
+					type: "error",
+					message: errs instanceof Array && errs.length > 0
+						? errs[0]
+						: getMessageIfExists(errs)
+							? getMessageIfExists(errs)
+							: "Ocorreu um erro desconhecido"
+				})
 			}
 
 			return false
 		}
 
 		return true
-	}, [])
+	}, [ getMessageIfExists, openSnackbar ])
 
 	return {
 		polyfill
